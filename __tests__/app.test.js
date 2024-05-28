54acc064245e63b7af532db33cbcbfeb58d4b176
Charlie White <163895848+charlie-mw@users.noreply.github.com>
2024-05-28 18:11:45 +0100

    Completing GET request for /api

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,18 +8,51 @@ beforeEach(() => seed(data));
 afterAll(() => db.end());
 
 describe("GET /api/topics", () => {
-    test("status 200: responds with all the topics", () => {
-        return request(app)
-        .get("/api/topics")
-        .expect(200)
-        .then((res) => {
-            expect(res.body.topics).toHaveLength(3);
-            res.body.topics.forEach((topic) => {
-                expect(topic).toMatchObject({
-                    description: expect.any(String),
-                    slug: expect.any(String)
-                })
-            })
-        })
-    })
-})
+  test("status 200: responds with all the topics", () => {
+    return request(app)
+      .get("/api/topics")
+      .expect(200)
+      .then(({ body }) => {
+        expect(body.topics).toHaveLength(3);
+        body.topics.forEach((topic) => {
+          expect(topic).toMatchObject({
+            description: expect.any(String),
+            slug: expect.any(String),
+          });
+        });
+      });
+  });
+});
+
+test("404: ERROR responds with an error when the path is invalid", () => {
+  return request(app)
+    .get("/api/nonsense")
+    .expect(404)
+    .then(({ body }) => {
+      expect(body.msg).toBe("Route not found");
+    });
+});
+
+describe("GET /api", () => {
+  test("status 200: responds with all the endpoints", () => {
+    return request(app)
+      .get("/api")
+      .expect(200)
+      .then(({ body }) => {
+        Object.entries(body).forEach(
+          ([endpointName, endpointDocumentation]) => {
+            const [httpMethod, path] = endpointName.split(" ");
+            expect(
+              ["GET", "PUT", "POST", "PATCH", "DELETE"].includes(httpMethod)
+            ).toEqual(true);
+            expect(path[0]).toEqual("/");
+            expect(endpointDocumentation).toMatchObject({
+              description: expect.any(String),
+              queries: expect.any(Array),
+              exampleResponse: expect.any(Object),
+            });
+          }
+        );
+      });
+  });
+});
--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const app = express();
 const { getTopics } = require("./controllers/api.controllers");
+const { getEndpoints } = require("./controllers/endpoints.controllers");
 
+
+app.get("/api", getEndpoints);
 app.get("/api/topics", getTopics)
 
+app.all("*", (req, res) => {
+  res.status(404).send({ msg: "Route not found" });
+});
+
 app.use((err, req, res, next) => {
-    if (err.status) {
-        res.status(err.status).send({ msg: err.msg })
-    }
-})
+  if (err.status) {
+    res.status(err.status).send({ msg: err.msg });
+  }
+});
+
 
 module.exports = app 
